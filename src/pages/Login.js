675cfe7b53c3a3be3488a138a24a810628b5b860
Ronald Lima <ronaldazevedolima@gmail.com>
2020-07-30 23:35:24 -0300

    tela login cc2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -52,8 +52,7 @@ class Login extends React.Component {
             name="email"
             data-testid="email-input"
             required
-            pattern=
-            "^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$"
+            pattern="^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})$"
           />
         </label>
         <label htmlFor="senha">
