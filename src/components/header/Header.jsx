eeb3cad170c918a45ecfc8865ac596c64d6ac240
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-08-03 17:51:13 -0300

    header + searchBar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,34 +1,57 @@
 import React from 'react';
 import ProfileIcon from '../../images/profileIcon.svg';
 import SearchIcon from '../../images/searchIcon.svg';
+import SearchBar from '../SearchBar/SearchBar';
 import './Header.css';
+import { Component } from 'react';
 
-const Header = () => {
-  function onClick() {
-    const searchBar = document.querySelector('.content');
-    if (searchBar.style.display === 'block') {
-      searchBar.style.display = 'none';
-    } else {
-      searchBar.style.display = 'block';
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+    }
+    this.setName = this.setName.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+   setName() {
+    if (document.URL.includes('comidas')) {
+      return <h1 data-testid="page-title">Comidas</h1>;
+    }
+    if (document.URL.includes('bebidas')) {
+      return <h1 data-testid="page-title">Bebidas</h1>;
+    }
+    if (document.URL.includes('perfil')) {
+      return <h1 data-testid="page-title">Perfil</h1>;
     }
   }
 
-  return (
-    <div>
-      <div className="header-container">
-        <a href="/perfil" data-testid="profile-top-btn">
-          <img src={ProfileIcon} alt="profile-icon" />
-        </a>
-        <h1 data-testid="page-title">Comidas</h1>
-        <button data-testid="search-top-btn" onClick={onClick}>
-          <img src={SearchIcon} alt="search-icon" />
-        </button>
+  onClick() {
+    const { show } = this.state;
+    if(show === false) {
+      this.setState({show: true});
+    } else {
+      this.setState({show: false});
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="header-container">
+          <a href="/perfil">
+            <img data-testid="profile-top-btn" src={ProfileIcon} alt="profile-icon" />
+          </a>
+          {this.setName()}
+          <button data-testid="search-top-btn" onClick={this.onClick}>
+            <img src={SearchIcon} alt="search-icon" />
+          </button>
+        </div>
+        {this.state.show && <SearchBar />}
       </div>
-      <div className="content">
-        <div>Alou</div>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Header;
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import SearchBar from '../components/SearchBar/SearchBar';
 import RenderFoods from '../components/RenderCards/RenderFoods';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/header/Header';
@@ -7,9 +6,7 @@ import Header from '../components/header/Header';
 const Foods = () => (
   <div>
     <Header />
-    <SearchBar />
     <RenderFoods />
-    <Header />
     <Footer />
   </div>
 );
