ab998729c3f49e19105923a9c78d010fc164e564
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-08-04 16:34:09 -0300

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,21 @@ import SearchIcon from '../../images/searchIcon.svg';
 import './Header.css';
 
 const Header = () => {
+  console.log(document.URL);
+  console.log(document.title);
+  
+  function setVisible() {
+    const url = document.URL;
+    const searchIc = document.querySelector('.search-icon');
+    console.log(searchIc);
+    if(url.includes('perfil')) {
+    return searchIc.style.display = 'none';
+  } else {
+    return  searchIc.style.display = 'block';
+  }
+  }
+  
+
   function onClick() {
     const searchBar = document.querySelector('.content');
     if (searchBar.style.display === 'block') {
@@ -20,7 +35,7 @@ const Header = () => {
           <img src={ProfileIcon} alt="profile-icon" />
         </a>
         <h1 data-testid="page-title">Comidas</h1>
-        <button data-testid="search-top-btn" onClick={onClick}>
+        <button className="search-icon" data-testid="search-top-btn" onClick={onClick}>
           <img src={SearchIcon} alt="search-icon" />
         </button>
       </div>
