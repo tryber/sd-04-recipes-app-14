230e9d226c5cd1a8acf3ac7b7932a9bf63f6c960
matheusysd <matheusyurisd@gmail.com>
2020-08-04 13:00:29 -0300

    Removendo renderização do botão de compartilhar

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -3,14 +3,12 @@ import SearchBar from '../components/SearchBar/SearchBar';
 import RenderFoods from '../components/RenderCards/RenderFoods';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/header/Header';
-import ShareButton from '../components/ShareButton/ShareButton';
 
 const Foods = () => (
   <div>
     <Header />
     <SearchBar />
     <RenderFoods />
-    <ShareButton />
     <Header />
     <Footer />
   </div>
