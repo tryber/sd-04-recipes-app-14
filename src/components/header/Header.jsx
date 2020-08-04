bdbbe0d1f0cd30848956e9f83834d9fbb23ca8de
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-08-04 15:25:39 -0300

    fixing cc

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,46 +1,49 @@
-import React, {Component} from 'react';
-import ProfileIcon from '../../images/profileIcon.svg';
-import SearchIcon from '../../images/searchIcon.svg';
-import SearchBar from '../SearchBar/SearchBar';
-import './Header.css';
+import React, { Component } from "react";
+import ProfileIcon from "../../images/profileIcon.svg";
+import SearchIcon from "../../images/searchIcon.svg";
+import SearchBar from "../SearchBar/SearchBar";
+import "./Header.css";
 
 class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
       show: false,
-    }
+    };
     this.setName = this.setName.bind(this);
     this.onClick = this.onClick.bind(this);
   }
 
-   setName() {
-    if (document.URL.includes('comidas')) {
+  onClick() {
+    const { show } = this.state;
+    if (show === false) {
+      this.setState({ show: true });
+    } else {
+      this.setState({ show: false });
+    }
+  }
+
+  setName() {
+    if (document.URL.includes("comidas")) {
       return <h1 data-testid="page-title">Comidas</h1>;
     }
-    if (document.URL.includes('bebidas')) {
+    if (document.URL.includes("bebidas")) {
       return <h1 data-testid="page-title">Bebidas</h1>;
     }
-    if (document.URL.includes('perfil')) {
+    if (document.URL.includes("perfil")) {
       return <h1 data-testid="page-title">Perfil</h1>;
     }
   }
-
-  onClick() {
-    const { show } = this.state;
-    if(show === false) {
-      this.setState({show: true});
-    } else {
-      this.setState({show: false});
-    }
-  }
-
   render() {
     return (
       <div>
         <div className="header-container">
           <a href="/perfil">
-            <img data-testid="profile-top-btn" src={ProfileIcon} alt="profile-icon" />
+            <img
+              data-testid="profile-top-btn"
+              src={ProfileIcon}
+              alt="profile-icon"
+            />
           </a>
           {this.setName()}
           <button data-testid="search-top-btn" onClick={this.onClick}>
@@ -51,6 +54,6 @@ class Header extends Component {
       </div>
     );
   }
-};
+}
 
 export default Header;
