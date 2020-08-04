c75eb150f8c6f96fb4746a5644c79c6d372feacf
matheusysd <matheusyurisd@gmail.com>
2020-08-04 16:21:45 -0300

    Corrigindo erros do footer, src das imgs errados

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import DrinkLogo from '../../images/drinkIcon.svg';
-import ExploreIcon from '../../images/exploreIcon.svg';
-import MealLogo from '../../images/mealIcon.svg';
+import drinkIcon from '../../images/drinkIcon.svg';
+import exploreIcon from '../../images/exploreIcon.svg';
+import mealIcon from '../../images/mealIcon.svg';
 import './Footer.css';
 
 const Footer = () => (
   <footer className="footer" data-testid="footer">
     <Link to="/bebidas" data-testid="drinks-bottom-btn">
-      <img src={DrinkLogo} alt="Drink logo" />
+      <img src={drinkIcon} alt="Drink logo" />
     </Link>
     <Link to="/explorar" data-testid="explore-bottom-btn">
-      <img src={ExploreIcon} alt="Explore logo" />
+      <img src={exploreIcon} alt="Explore logo" />
     </Link>
     <Link to="/comidas" data-testid="food-bottom-btn">
-      <img src={MealLogo} alt="Meal logo" />
+      <img src={mealIcon} alt="Meal logo" />
     </Link>
   </footer>
 );
