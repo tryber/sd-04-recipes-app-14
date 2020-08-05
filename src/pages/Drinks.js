c2f6319b3cddd1c3a74eee84b32c463ae14b3a1a
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-08-05 17:06:16 -0300

    Carousel Finalizado, basta renderizar

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -2,14 +2,12 @@ import React from 'react';
 import RenderDrinks from '../components/RenderCards/RenderDrinks';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/header/Header';
-import Carousel from '../components/Carousel/Carousel';
 
 const Drinks = () => (
   <div>
     <Header />
     <div>
       <RenderDrinks />
-      <Carousel />
     </div>
     <Footer />
   </div>
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -2,14 +2,12 @@ import React from 'react';
 import RenderFoods from '../components/RenderCards/RenderFoods';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/header/Header';
-import Carousel from '../components/Carousel/Carousel';
 
 const Foods = () => (
   <div>
     <Header />
     <div>
       <RenderFoods />
-      <Carousel />
     </div>
     <Footer />
   </div>
