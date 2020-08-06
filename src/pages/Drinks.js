0d8cf456a8931a7725704878ba3bce4b363e2827
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-08-06 17:45:44 -0300

    kk

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -2,11 +2,14 @@ import React from 'react';
 import RenderDrinks from '../components/RenderCards/RenderDrinks';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/header/Header';
+import './Styles/Cards.css';
 
 const Drinks = () => (
   <div>
     <Header />
-    <RenderDrinks />
+    <div className="div-page-cards">
+      <RenderDrinks />
+    </div>
     <Footer />
   </div>
 );
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -2,11 +2,14 @@ import React from 'react';
 import RenderFoods from '../components/RenderCards/RenderFoods';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/header/Header';
+import './Styles/Cards.css';
 
 const Foods = () => (
   <div>
     <Header />
-    <RenderFoods />
+    <div className="div-page-cards">
+      <RenderFoods />
+    </div>
     <Footer />
   </div>
 );
