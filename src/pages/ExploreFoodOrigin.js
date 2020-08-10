61c15d66ec6f0dcf9666a4386eb68d8563fbc912
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-08-10 16:49:52 -0300

    cc fixed

--- src/pages/ExploreFoodOrigin.js
+++ src/pages/ExploreFoodOrigin.js
@@ -3,14 +3,12 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 
-const ExploreFoodOrigin = ({ search = true, name = 'Explorar Origem' }) => {
-  return (
-    <div>
-      <Header search={search} name={name} />
-      <Footer />
-    </div>
-  );
-};
+const ExploreFoodOrigin = ({ search = true, name = 'Explorar Origem' }) => (
+  <div>
+    <Header search={search} name={name} />
+    <Footer />
+  </div>
+);
 
 ExploreFoodOrigin.propTypes = {
   search: PropTypes.bool.isRequired,
