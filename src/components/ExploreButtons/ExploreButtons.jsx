86094b32cb946462834cf06e46b9089f59d597d9
matheusysd <matheusyurisd@gmail.com>
2020-08-11 16:55:29 -0300

    Explore Buttons

--- src/components/ExploreButtons/ExploreButtons.jsx
+++ src/components/ExploreButtons/ExploreButtons.jsx
@@ -1,8 +1,19 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 const ExploreButtons = ({ type }) => {
   const history = useHistory();
+  const getRandomRecipe = () => {
+    if (document.URL.includes('comidas')) {
+      return fetch('https://www.themealdb.com/api/json/v1/1/random.php').then((data) =>
+        data.json().then((json) => history.push(`/comidas/${json.meals[0].idMeal}`)),
+      );
+    }
+    return fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php').then((data) =>
+      data.json().then((json) => history.push(`/bebidas/${json.drinks[0].idDrink}`)),
+    );
+  };
   const redirectTo = (path) => {
     history.push(`/explorar/${type}/${path}`);
   };
@@ -28,17 +39,15 @@ const ExploreButtons = ({ type }) => {
           Por Local de Origem
         </button>
       )}
-      <button
-        type="button"
-        onClick={() => {
-          redirectTo();
-        }}
-        data-testid="explore-surprise"
-      >
-        Me surpreenda!
+      <button type="button" onClick={() => getRandomRecipe()} data-testid="explore-surprise">
+        Me Surpreenda!
       </button>
     </div>
   );
+};
+
+ExploreButtons.propTypes = {
+  type: PropTypes.string.isRequired,
 };
 
 export default ExploreButtons;
