95b2c516efdf87204ba1df1c327dd57a4deb1656
Caio Ribeiro <cacoribeiro@pop-os.localdomain>
2020-08-12 20:34:56 -0300

    Arrumando

--- src/components/DoneRecipes/DoneDrinks.jsx
+++ src/components/DoneRecipes/DoneDrinks.jsx
@@ -9,8 +9,8 @@ function DoneDrinks(props) {
   console.log(id, name, image, doneDate);
 
   return (
-    <Link to={`/bebidas/${id}`}>
-      <div>
+    <div>
+      <Link to={`/bebidas/${id}`}>
         <img
           data-testid={`${index}-horizontal-image`}
           style={{ height: 200 }}
@@ -20,12 +20,12 @@ function DoneDrinks(props) {
         <p data-testid={`${index}-horizontal-top-text`}>{alcoholicOrNot}</p>
         <h2 data-testid={`${index}-horizontal-name`}>{name}</h2>
         <p data-testid={`${index}-horizontal-done-date`}>{doneDate}</p>
-        <ShareButtonByURL
-          URL={`http://localhost:3000/bebidas/${id}`}
-          dataTestId={`${index}-horizontal-share-btn`}
-        />
-      </div>
-    </Link>
+      </Link>
+      <ShareButtonByURL
+        URL={`http://localhost:3000/bebidas/${id}`}
+        dataTestId={`${index}-horizontal-share-btn`}
+      />
+    </div>
   );
 }
 
