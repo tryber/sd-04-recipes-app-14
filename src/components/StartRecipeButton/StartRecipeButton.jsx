8848ccf697169294d299dcd23ed5d15b4788f5a7
matheusysd <matheusyurisd@gmail.com>
2020-08-20 14:10:17 -0300

    criando pr

--- src/components/StartRecipeButton/StartRecipeButton.jsx
+++ src/components/StartRecipeButton/StartRecipeButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { passRecipe } from '../../actions/actions';
-
+// Alo commit
 class StartRecipeButton extends React.Component {
   static handleBotao(id) {
     if (localStorage.doneRecipes) {
